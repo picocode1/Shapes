26b5600a4a4132ad994ce397793778d003e88503
picocode <38665815+picocode1@users.noreply.github.com>
2022-05-24 12:38:14 +0200

    Update shape.js

--- shape.js
+++ shape.js
@@ -1,40 +1,42 @@
 const readline = require('readline');
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+	input: process.stdin,
+	output: process.stdout
 });
 
 function DiamondAnimation(n, Reverse, speed) {
-	if (Reverse){
+	if (Reverse) {
 		for (let i = 0; i < n; i++) {
-			setTimeout(()=> {
+			setTimeout(() => {
 				console.log(`${" ".repeat(n - i - 1)}${"#".repeat(i + i)}`)
 			}, i * speed)
 		}
-		setTimeout(()=> {
+		setTimeout(() => {
 			DiamondAnimation(n, false, speed)
 		}, speed * n)
-	}
-	else
-	{
+	} else {
 		for (let i = n - 1; i >= 0; i--) {
-			setTimeout(()=> {
-				console.log(`${" ".repeat(i + 1)}${"#".repeat(n - i - 1)}${"#".repeat(n - i -1)}`)
-				// console.log(`${"#".repeat(i + n)}${" ".repeat(n + 1)}`)
+			setTimeout(() => {
+				try {
+					console.log(`${" ".repeat(i + 1)}${"#".repeat(n - i - 3)}${"#".repeat(n - i -1)}`)
+					// console.log(`${"#".repeat(i + n)}${" ".repeat(n + 1)}`)
+				} catch (e) {}
+
 			}, i * speed)
 		}
-		setTimeout(()=> {
+		setTimeout(() => {
 			DiamondAnimation(n, true, speed)
 		}, speed * n)
 	}
 
 }
-// var shouldSwitch = false
-// var stairValue = 40
+// var stairValue = 10
 // DiamondAnimation(stairValue, true, (stairValue / stairValue) * 25)
 
 
 function square(width, height) {
+	// console.clear()
+
 	console.log(`${"#".repeat(width)}`)
 	for (let i = 0; i < height; i++) {
 		console.log(`#${" ".repeat(width - 2)}#`)
@@ -47,6 +49,8 @@ function square(width, height) {
 
 
 function PyramidSquare(width, height) {
+	// console.clear()
+
 	console.log(`${"#".repeat(width * 2)}`)
 	for (let i = 1; i < height - 1; i++) {
 		console.log(`#${" ".repeat(width - i )}${"@".repeat(i + i - 1)}${" ".repeat(width - i - 1)}#`)
@@ -59,31 +63,35 @@ function PyramidSquare(width, height) {
 
 
 var options = ["PyramidSquare", "Square", "DiamondAnimation"]
+let Question = () => {
+	console.clear()
+	rl.question(`Choose pattern: ${options.join(', ')}\n`, function (pattern) {
+		if (!pattern) Question()
+		rl.question('Select size: \n', function (size) {
+			if (!size) {
+				size = 20
+			}
+			size = Number(size)
 
-rl.question(`Choose pattern: ${options.join(', ')}\n`, function (pattern) {
-  rl.question('Select size: \n', function (size) {
-	if (!size){
-		size = 20
-	}
-	size = Number(size)
+			switch (pattern.toLowerCase()) {
+				case "pyramidsquare":
+					PyramidSquare(size, size / 2)
+					break;
 
-    switch(pattern.toLowerCase()) {
-		case "pyramidsquare":
-			PyramidSquare(size, size / 2)
-			break;
+				case "square":
+					square(size, size / 2)
+					break;
 
-		case "square":
-			square(size, size / 2)
-			break;
+				case "diamondanimation":
+					DiamondAnimation(size, true, (size / size) * 25)
+					break;
 
-		case "diamondanimation":
-			DiamondAnimation(size, true, (size / size) * 25)
-			break;
-
-		default:
-			console.log("Invalid input")
-			break;
-	}
-    rl.close();
-  });
-});
+				default:
+					console.log("Invalid input")
+					break;
+			}
+			rl.close();
+		});
+	});
+}
+Question()
